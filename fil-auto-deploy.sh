226057990db317aa2e1df99dd326137e88e66e06
ncgege <8160903@qq.com>
2021-04-18 13:15:16 +0800

    Update fil-auto-deploy.sh

--- fil-auto-deploy.sh
+++ fil-auto-deploy.sh
@@ -136,10 +136,12 @@ source /etc/profile
 
 mk_wk_run_sh() {
 if [[ ! -d "/ipfs/filecoin/lotusworker1" ]]; then
-    mkdir -p /ipfs/filecoin/lotusworker1
+    mkdir -p /ipfs/filecoin/lotusworker1 /ipfs/filecoin/tmpdir1 /ipfs/filecoin/tmpdir2
 fi
 if [[ ! -f "/filestar/lotusworker/" ]]; then
 cd /ipfs/filecoin/lotusworker1 && wget https://github.com/ncgege/fil-auto-deploy/run-fil-wk-moran-p1p2.sh ./
+cd /ipfs/filecoin/ && wget https://github.com/ncgege/fil-auto-deploy/killworker.sh ./
+wget https://github.com/ncgege/fil-auto-deploy/wk-ap-switch.sh ./
 fi
 }
 
